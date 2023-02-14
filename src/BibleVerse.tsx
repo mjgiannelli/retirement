5219ee2ab0aa0598cc64c64062c6b1ca51310f52
Mark Giannelli <giannelli.mark@gmail.com>
2023-02-13 21:20:25 -0500

    update verse interface

--- src/BibleVerse.tsx
+++ src/BibleVerse.tsx
@@ -1,29 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const BibleVerse: React.FC = () => {
-    interface Verse {
-        bookname: string,
-        chapter: string,
-        text: string,
-        verse: string
-    }
-  const [verse, setVerse] = useState<Verse>();
+  interface Verse {
+    bookname: string;
+    chapter: string;
+    text: string;
+    verse: string;
+  }
+  const [verse, setVerse] = useState<Verse[]>();
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch('https://labs.bible.org/api/?passage=votd&type=json');
+      const response = await fetch(
+        "https://labs.bible.org/api/?passage=votd&type=json"
+      );
       const data = await response.json();
-      setVerse(data[0]);
+      setVerse(data);
     };
     fetchData();
   }, []);
 
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>Verse of the day 😇</h1>
-      <p style={{ fontSize: '1.5rem', fontWeight: 'bold',  }}>{verse?.bookname} <span style={{color:'red'}}>{verse?.chapter}</span> : <span style={{color:'green'}}>{verse?.verse}</span></p>
-      <p style={{ fontSize: '1.25rem' }}>{verse?.text}</p>
-    </div>
+    <>
+    <h1>Verse of the day 😇</h1>
+      {verse?.map((verse) => {
+        return (
+          <div style={{ textAlign: "center" }}>
+            <p style={{ fontSize: "1.5rem", fontWeight: "bold" }}>
+              {verse?.bookname}{" "}
+              <span style={{ color: "red" }}>{verse?.chapter}</span> :{" "}
+              <span style={{ color: "green" }}>{verse?.verse}</span>
+            </p>
+            <p style={{ fontSize: "1.25rem" }}>{verse?.text}</p>
+          </div>
+        );
+      })}
+    </>
   );
 };
 
